e1fd5f7be2d37cedd603736bec7319f73ba44c41
Stuart MacKay <smackay@flagstonesoftware.com>
2025-01-19 08:52:16 +0000

    project: Add django-autocomplete-light

--- config/settings.py
+++ config/settings.py
@@ -47,6 +47,8 @@ if DJANGO_ENV == "production" and DEBUG:
 # #####################
 
 INSTALLED_APPS = [
+    "dal",
+    "dal_select2",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
