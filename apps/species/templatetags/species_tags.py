e3c420797dc03372642984edc2ee6f94a20f3550
Stuart MacKay <smackay@flagstonesoftware.com>
2025-04-04 18:43:46 +0100

    Fix: Display the common_name field is a translation is not available

--- apps/species/templatetags/species_tags.py
+++ apps/species/templatetags/species_tags.py
@@ -5,4 +5,7 @@ register = template.Library()
 
 @register.simple_tag()
 def get_common_name(species, language_code):
-    return species.data["common_name"][language_code]
+    if "common_name" in species.data:
+        if language_code in species.data["common_name"]:
+            return species.data["common_name"][language_code]
+    return species.common_name
