4e307a7c570af7e7f81dac9003a94252c37dd14e
Stuart MacKay <smackay@flagstonesoftware.com>
2025-05-22 18:53:41 +0100

    Use 'pattern_name' instead of 'url' when redirecting to the New Page
    When using reverse_lazy, any accentated characters are escaped. A
    problem occurs as django uses the % operator on the url to apply any
    kwargs, so the escaped characters trigger an invalid format character
    error.

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,12 @@
 from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
-from django.urls import include, path, reverse_lazy
+from django.urls import include, path
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import RedirectView
 
 urlpatterns = i18n_patterns(
-    path("", RedirectView.as_view(url=reverse_lazy("news:latest")), name="index"),
+    path("", RedirectView.as_view(pattern_name="news:latest"), name="index"),
     path(_("autocomplete/"), include("data.urls")),
     path(_("news/"), include("news.urls")),
     path(_("checklists/"), include("checklists.urls")),
