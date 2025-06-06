69793a8944ed87778b87abb9fc031d76aea5f01d
Stuart MacKay <smackay@flagstonesoftware.com>
2025-06-06 15:24:26 +0100

    News, Checklists, Observations - add tests for page query parameters

--- tests/unit/test_monthly_news_page.py
+++ tests/unit/test_monthly_news_page.py
@@ -2,18 +2,71 @@ import datetime as dt
 
 from django.urls import reverse
 
+import pytest
 
-def test_weekly_news_page__current_week__displayed(db_no_rollback, client):
-    response = client.get(reverse("news:monthly"))
+
+@pytest.fixture
+def latest_month():
+    return reverse("news:monthly")
+
+
+@pytest.fixture
+def selected_month():
+    today = dt.date.today()
+    return reverse("news:for-month", kwargs={"year": today.year, "month": today.month})
+
+
+def test_monthly_news_page__latest_month(  # noqa
+    db_no_rollback, client, latest_month
+):
+    response = client.get(latest_month)
     assert response.status_code == 200
 
 
-def test_weekly_news_page__selected_week__displayed(db_no_rollback, client):
-    today = dt.date.today()
-    response = client.get(
-        reverse(
-            "news:for-month",
-            kwargs={"year": today.year, "month": today.month},
-        )
-    )
+def test_monthly_news_page__latest_month__search_by_country(
+    db_no_rollback, client, latest_month, search_by_country
+):
+    response = client.get("%s?%s" % (latest_month, search_by_country))
+    assert response.status_code == 200
+
+
+def test_monthly_news_page__latest_month__search_by_state(
+    db_no_rollback, client, latest_month, search_by_state
+):
+    response = client.get("%s?%s" % (latest_month, search_by_state))
+    assert response.status_code == 200
+
+
+def test_monthly_news_page__latest_month__search_by_county(
+    db_no_rollback, client, latest_month, search_by_county
+):
+    response = client.get("%s?%s" % (latest_month, search_by_county))
+    assert response.status_code == 200
+
+
+def test_monthly_news_page__selected_month(  # noqa
+    db_no_rollback, client, selected_month
+):
+    response = client.get(selected_month)
+    assert response.status_code == 200
+
+
+def test_monthly_news_page__selected_month__search_by_country(
+    db_no_rollback, client, selected_month, search_by_country
+):
+    response = client.get("%s?%s" % (selected_month, search_by_country))
+    assert response.status_code == 200
+
+
+def test_monthly_news_page__selected_month__search_by_state(
+    db_no_rollback, client, selected_month, search_by_state
+):
+    response = client.get("%s?%s" % (selected_month, search_by_state))
+    assert response.status_code == 200
+
+
+def test_monthly_news_page__selected_month__search_by_county(
+    db_no_rollback, client, selected_month, search_by_county
+):
+    response = client.get("%s?%s" % (selected_month, search_by_county))
     assert response.status_code == 200
--- tests/unit/test_observations_page.py
+++ tests/unit/test_observations_page.py
@@ -3,6 +3,10 @@ from django.urls import reverse
 import pytest
 
 
+def has_pages(response) -> bool:
+    return response.context["page_obj"].paginator.num_pages > 0
+
+
 @pytest.fixture
 def url():
     return reverse("observations:list")
@@ -11,3 +15,66 @@ def url():
 def test_observations_page__displayed(db_no_rollback, client, url):
     response = client.get(url)
     assert response.status_code == 200
+
+
+def test_observations_page__filtered_by_country(
+    db_no_rollback, client, url, filter_by_country
+):
+    response = client.get("%s?%s" % (url, filter_by_country))
+    assert has_pages(response)
+
+
+def test_observations_page__filtered_by_state(
+    db_no_rollback, client, url, filter_by_state
+):
+    response = client.get("%s?%s" % (url, filter_by_state))
+    assert has_pages(response)
+
+
+def test_observations_page__filtered_by_county(
+    db_no_rollback, client, url, filter_by_county
+):
+    response = client.get("%s?%s" % (url, filter_by_county))
+    assert has_pages(response)
+
+
+def test_observations_page__filtered_by_observer(
+    db_no_rollback, client, url, filter_by_observer
+):
+    response = client.get("%s?%s" % (url, filter_by_observer))
+    assert has_pages(response)
+
+
+def test_observations_page__filtered_by_species(
+    db_no_rollback, client, url, filter_by_species
+):
+    response = client.get("%s?%s" % (url, filter_by_species))
+    assert has_pages(response)
+
+
+def test_observations_page__filtered_by_start_date(
+    db_no_rollback, client, url, filter_by_start_date
+):
+    response = client.get("%s?%s" % (url, filter_by_start_date))
+    assert has_pages(response)
+
+
+def test_observations_page__filtered_by_end_date(
+    db_no_rollback, client, url, filter_by_end_date
+):
+    response = client.get("%s?%s" % (url, filter_by_end_date))
+    assert has_pages(response)
+
+
+def test_observations_page__filtered_by_date_range(
+    db_no_rollback, client, url, filter_by_date_range
+):
+    response = client.get("%s?%s" % (url, filter_by_date_range))
+    assert has_pages(response)
+
+
+def test_observations_page__ordered_by_count(
+    db_no_rollback, client, url, ordered_by_count
+):
+    response = client.get("%s?%s" % (url, ordered_by_count))
+    assert has_pages(response)
