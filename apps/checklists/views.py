15ce8adc0c6ea406d53c07721763d47821fd3eed
Stuart MacKay <smackay@flagstonesoftware.com>
2025-06-21 17:46:38 +0100

    Move the filter form mixins and filter view into the data app

--- apps/checklists/views.py
+++ apps/checklists/views.py
@@ -7,47 +7,7 @@ from django.views import generic
 
 from checklists.forms import ChecklistFilterForm
 from data.models import Checklist, Country
-
-
-class FilteredListView(generic.list.MultipleObjectMixin, generic.FormView):
-    related = None
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.form = None
-
-    def get_related(self):
-        return self.related
-
-    def get_ordering(self):
-        ordering = super().get_ordering()
-        if value := self.form.get_ordering():
-            ordering = value
-        return ordering
-
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        if filters := self.form.get_filters():
-            queryset = queryset.filter(filters)
-        if related := self.get_related():
-            queryset = queryset.select_related(*related)
-        return queryset
-
-    def get_form_kwargs(self):
-        kwargs = super().get_form_kwargs()
-        kwargs["data"] = self.request.GET
-        return kwargs
-
-    def get_form(self, form_class=None):
-        form = super().get_form(form_class=form_class)
-        form.is_valid()
-        return form
-
-    def get(self, request, *args, **kwargs):
-        self.form = self.get_form()
-        objects = self.get_queryset()
-        context = self.get_context_data(object_list=objects, form=self.form)
-        return self.render_to_response(context)
+from data.views import FilteredListView
 
 
 class ChecklistsView(FilteredListView):
