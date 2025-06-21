80b7f37e68116bb5b99dc4e533bce32421b6bd28
Stuart MacKay <smackay@flagstonesoftware.com>
2025-06-21 16:00:59 +0100

    Checklists Filter - factor out a reusable filter list view

--- apps/checklists/views.py
+++ apps/checklists/views.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.shortcuts import get_object_or_404
-from django.urls import reverse
+from django.urls import reverse_lazy
 from django.utils import translation
 from django.utils.functional import cached_property
 from django.views import generic
@@ -9,25 +9,21 @@ from checklists.forms import ChecklistFilterForm
 from data.models import Checklist, Country
 
 
-class ChecklistsView(generic.list.MultipleObjectMixin, generic.FormView):
-    form_class = ChecklistFilterForm
-    model = Checklist
-    paginate_by = 50
-    template_name = "checklists/list.html"
+class FilteredListView(generic.list.MultipleObjectMixin, generic.FormView):
+    related = None
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.form = None
 
-    @cached_property
-    def show_country(self):
-        return Country.objects.all().count() > 1
+    def get_related(self):
+        return self.related
 
     def get_ordering(self):
-        return self.form.get_ordering()
-
-    def get_related(self):  # noqa
-        return ["country", "state", "county", "location", "observer"]
+        ordering = super().get_ordering()
+        if value := self.form.get_ordering():
+            ordering = value
+        return ordering
 
     def get_queryset(self):
         queryset = super().get_queryset()
@@ -40,7 +36,6 @@ class ChecklistsView(generic.list.MultipleObjectMixin, generic.FormView):
     def get_form_kwargs(self):
         kwargs = super().get_form_kwargs()
         kwargs["data"] = self.request.GET
-        kwargs["show_country"] = self.show_country
         return kwargs
 
     def get_form(self, form_class=None):
@@ -48,25 +43,47 @@ class ChecklistsView(generic.list.MultipleObjectMixin, generic.FormView):
         form.is_valid()
         return form
 
-    @staticmethod
-    def get_translations():
-        urls = []
-        for code, name in settings.LANGUAGES:
-            with translation.override(code):
-                urls.append((reverse("checklists:list"), name))
-        return urls
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["show_country"] = self.show_country
-        context["translations"] = self.get_translations()
-        return context
-
     def get(self, request, *args, **kwargs):
         self.form = self.get_form()
         objects = self.get_queryset()
         context = self.get_context_data(object_list=objects, form=self.form)
         return self.render_to_response(context)
+
+
+class ChecklistsView(FilteredListView):
+    form_class = ChecklistFilterForm
+    model = Checklist
+    paginate_by = 50
+    template_name = "checklists/list.html"
+    url = reverse_lazy("checklists:list")
+
+    def get_url(self):
+        return self.url
+
+    @cached_property
+    def show_country(self):
+        return Country.objects.all().count() > 1
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs["show_country"] = self.show_country
+        return kwargs
+
+    def get_related(self):  # noqa
+        return ["country", "state", "county", "location", "observer"]
+
+    def get_translated_urls(self):
+        urls = []
+        for code, name in settings.LANGUAGES:
+            with translation.override(code):
+                urls.append((self.get_url(), name))
+        return urls
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["show_country"] = self.show_country
+        context["translations"] = self.get_translated_urls()
+        return context
 
 
 class DetailView(generic.DetailView):
