1e9652d891c83778c698f4d3035f9431cc375c2b
Stuart MacKay <smackay@flagstonesoftware.com>
2025-06-28 08:07:44 +0100

    Open filter panels to show selected values

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,7 +13,59 @@ function remove_blank_form_fields() {
     }
 }
 
+function open_filter_panels() {
+    const params = new Proxy(new URLSearchParams(window.location.search), {
+        get: (searchParams, prop) => searchParams.get(prop),
+    });
+
+    if (params.country || params.state || params.country || params.location || params.hotspot) {
+        const list = document.querySelectorAll('a[href="#form-location"]');
+        for (const link of list) {
+            link.click();
+        }
+    }
+    if (params.observer) {
+        const list = document.querySelectorAll('a[href="#form-observer"]');
+        for (const link of list) {
+            link.click();
+        }
+    }
+    if (params.from || params.until) {
+        const list = document.querySelectorAll('a[href="#form-date-range"]');
+        for (const link of list) {
+            link.click();
+        }
+    }
+    if (params.species) {
+        const list = document.querySelectorAll('a[href="#form-species"]');
+        for (const link of list) {
+            link.click();
+        }
+    }
+    if (params.category) {
+        const list = document.querySelectorAll('a[href="#form-category"]');
+        for (const link of list) {
+            link.click();
+        }
+    }
+    if (params.order) {
+        const checklist_list = document.querySelectorAll('a[href="#form-checklist-order"]');
+        for (const link of checklist_list) {
+            link.click();
+        }
+        const observation_list = document.querySelectorAll('a[href="#form-checklist-order"]');
+        for (const link of observation_list) {
+            link.click();
+        }
+        const seen_list = document.querySelectorAll('a[href="#form-checklist-order"]');
+        for (const link of seen_list) {
+            link.click();
+        }
+    }
+}
+
 document.addEventListener('DOMContentLoaded', (event) => {
   'use strict';
+  open_filter_panels();
   remove_blank_form_fields();
 });
