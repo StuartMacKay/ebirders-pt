ac35b825f4c00ec6a2b900b3012b0746c3f483e8
Stuart MacKay <smackay@flagstonesoftware.com>
2025-06-29 18:34:24 +0100

    Add page caching for Checklists, Observations, and Species lists

--- apps/checklists/urls.py
+++ apps/checklists/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path, re_path
+from django.views.decorators.cache import cache_page
 
 from .views import (
     ChecklistsView,
     DetailView,
 )
 
+# Cache expiration intervals
+minutes: int = 60
+hours: int = 60 * minutes
+days: int = 24 * hours
+
 app_name = "checklists"
 
 urlpatterns = [
-    path("", ChecklistsView.as_view(), name="list"),
+    path("",  cache_page(1 * hours)(ChecklistsView.as_view()), name="list"),
     re_path(r"^(?P<identifier>S\d+)/$", DetailView.as_view(), name="detail"),
 ]
--- apps/observations/urls.py
+++ apps/observations/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from .views import ObservationsView
+
+# Cache expiration intervals
+minutes: int = 60
+hours: int = 60 * minutes
+days: int = 24 * hours
 
 app_name = "observations"
 
 urlpatterns = [
-    path("", ObservationsView.as_view(), name="list"),
+    path("",  cache_page(1 * hours)(ObservationsView.as_view()), name="list"),
 ]
