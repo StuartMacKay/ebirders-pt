b01414709a5afebcb65f735375ddf3f7068d14df
Stuart MacKay <smackay@flagstonesoftware.com>
2025-07-04 11:32:47 +0100

    Forms - update tests for multiselect fields

--- apps/data/tests/test_forms.py
+++ apps/data/tests/test_forms.py
@@ -1,140 +1,140 @@
 from django.db.models import Q
+from django.http import QueryDict
 
 from data.forms import (
     CategoryFilter,
     ChecklistOrder,
     DateRangeFilter,
-    HotspotFilter,
     LocationFilter,
     ObservationOrder,
     ObserverFilter,
-    SeenOrder,
+    SpeciesOrder,
 )
 
 
 def test_country_field__with_valid_code__form_is_valid(country):
-    form = LocationFilter(show_country=True, data={"country": country.code})
+    form = LocationFilter(show_country=True, data=QueryDict("country=%s" % country.code))
     assert form.is_valid()
 
 
 def test_country_field__with_invalid_code__form_is_invalid(country):
-    form = LocationFilter(show_country=True, data={"country": country.code[::-1]})
+    form = LocationFilter(show_country=True, data=QueryDict("country=%s" % country.code[::-1]))
     assert not (form.is_valid())
 
 
 def test_country_field__with_invalid_code__field_error_reported(country):
-    form = LocationFilter(show_country=True, data={"country": country.code[::-1]})
+    form = LocationFilter(show_country=True, data=QueryDict("country=%s" % country.code[::-1]))
     form.is_valid()
     assert "country" in form.errors
 
 
 def test_country_field__with_lowercase_code__field_error_reported(country):
-    form = LocationFilter(show_country=True, data={"country": country.code.lower()})
+    form = LocationFilter(show_country=True, data=QueryDict("country=%s" % country.code.lower()))
     form.is_valid()
     assert "country" in form.errors
 
 
 def test_country_field__with_valid_code__filter_is_defined(country):
-    form = LocationFilter(show_country=True, data={"country": country.code})
+    form = LocationFilter(show_country=True, data=QueryDict("country=%s" % country.code))
     form.is_valid()
     assert Q(country__code=country.code) == form.get_filters()
 
 
 def test_state_field__with_valid_code__form_is_valid(state):
-    form = LocationFilter(show_country=False, data={"state": state.code})
+    form = LocationFilter(show_country=False, data=QueryDict("state=%s" % state.code))
     assert form.is_valid()
 
 
 def test_state_field__with_invalid_code__form_is_invalid(state):
-    form = LocationFilter(show_country=False, data={"state": state.code[::-1]})
+    form = LocationFilter(show_country=False, data=QueryDict("state=%s" % state.code[::-1]))
     assert not form.is_valid()
 
 
 def test_state_field__with_invalid_code__field_error_reported(state):
-    form = LocationFilter(show_country=False, data={"state": state.code[::-1]})
+    form = LocationFilter(show_country=False, data=QueryDict("state=%s" % state.code[::-1]))
     form.is_valid()
     assert "state" in form.errors
 
 
 def test_state_field__with_lowercase_code__field_error_reported(state):
-    form = LocationFilter(show_country=False, data={"state": state.code.lower()})
+    form = LocationFilter(show_country=False, data=QueryDict("state=%s" % state.code.lower()))
     form.is_valid()
     assert "state" in form.errors
 
 
-def test_state_field__with_other_country__field_error_reported(country, state):
+def test_state_field__with_other_country__field_error_reported(country):
     form = LocationFilter(
-        show_country=True, data={"country": country.code[::-1], "state": state.code}
+        show_country=True, data=QueryDict("country=%s&state=AA" % country.code)
     )
     form.is_valid()
     assert "state" in form.errors
 
 
 def test_state_field__with_valid_code__filter_is_defined(state):
-    form = LocationFilter(show_country=False, data={"state": state.code})
+    form = LocationFilter(show_country=False, data=QueryDict("state=%s" % state.code))
     form.is_valid()
-    assert Q(state__code=state.code) == form.get_filters()
+    assert Q(state__code__in=[state.code]) == form.get_filters()
 
 
 def test_county_field__with_valid_code__form_is_valid(county):
-    form = LocationFilter(show_country=False, data={"county": county.code})
+    form = LocationFilter(show_country=False, data=QueryDict("county=%s" % county.code))
     assert form.is_valid()
 
 
 def test_county_field__with_invalid_code__form_is_invalid(county):
-    form = LocationFilter(show_country=False, data={"county": county.code[::-1]})
+    form = LocationFilter(show_country=False, data=QueryDict("county=%s" % county.code[::-1]))
     assert not form.is_valid()
 
 
 def test_county_field__with_invalid_code__field_error_reported(county):
-    form = LocationFilter(show_country=False, data={"county": county.code[::-1]})
+    form = LocationFilter(show_country=False, data=QueryDict("county=%s" % county.code[::-1]))
     form.is_valid()
     assert "county" in form.errors
 
 
 def test_county_field__with_lowercase_code__field_error_reported(county):
-    form = LocationFilter(show_country=False, data={"county": county.code.lower()})
+    form = LocationFilter(show_country=False, data=QueryDict("county=%s" % county.code.lower()))
     form.is_valid()
     assert "county" in form.errors
 
 
-def test_county_field__with_other_country__field_error_reported(country, county):
+def test_county_field__with_other_country__field_error_reported(country):
     form = LocationFilter(
-        show_country=True, data={"country": country.code[::-1], "county": county.code}
+        show_country=True, data=QueryDict("country=%s&county=AA" % country.code)
     )
     form.is_valid()
     assert "county" in form.errors
 
 
-def test_county_field__with_other_state__field_error_reported(state, county):
+def test_county_field__with_other_state__field_error_reported(state):
     form = LocationFilter(
-        show_country=False, data={"state": state.code[::-1], "county": county.code}
+        show_country=False, data=QueryDict("state=%s&county=AA" % state)
     )
     form.is_valid()
     assert "county" in form.errors
 
 
 def test_county_field__with_valid_code__filter_is_defined(county):
-    form = LocationFilter(show_country=False, data={"county": county.code})
+    form = LocationFilter(show_country=False, data=QueryDict("county=%s" % county.code))
     form.is_valid()
-    assert Q(county__code=county.code) == form.get_filters()
+    assert Q(county__code__in=[county.code]) == form.get_filters()
 
 
 def test_location_field__with_valid_code__form_is_valid(location):
-    form = LocationFilter(show_country=False, data={"location": location.identifier})
+    form = LocationFilter(show_country=False, data=QueryDict("location=%s" % location.identifier))
     assert form.is_valid()
 
 
 def test_location_field__with_invalid_code__form_is_invalid(location):
     form = LocationFilter(
-        show_country=False, data={"location": location.identifier[::-1]}
+        show_country=False, data=QueryDict("location=%s" % location.identifier[::-1])
     )
     assert not form.is_valid()
 
 
 def test_location_field__with_invalid_code__field_error_reported(location):
     form = LocationFilter(
-        show_country=False, data={"location": location.identifier[::-1]}
+        show_country=False, data=QueryDict("location=%s" % location.identifier[::-1])
     )
     form.is_valid()
     assert "location" in form.errors
@@ -142,16 +142,16 @@ def test_location_field__with_invalid_code__field_error_reported(location):
 
 def test_location_field__with_lowercase_code__field_error_reported(location):
     form = LocationFilter(
-        show_country=False, data={"location": location.identifier.lower()}
+        show_country=False, data=QueryDict("location=%s" % location.identifier.lower())
     )
     form.is_valid()
     assert "location" in form.errors
 
 
-def test_location_field__with_other_country__field_error_reported(country, location):
+def test_location_field__with_other_country__field_error_reported(country):
     form = LocationFilter(
         show_country=True,
-        data={"country": country.code[::-1], "location": location.identifier},
+        data=QueryDict("country=%s&location=L00" % country.code)
     )
     form.is_valid()
     assert "location" in form.errors
@@ -160,7 +160,7 @@ def test_location_field__with_other_country__field_error_reported(country, locat
 def test_location_field__with_other_state__field_error_reported(state, location):
     form = LocationFilter(
         show_country=False,
-        data={"state": state.code[::-1], "location": location.identifier},
+        data=QueryDict("state=%s&location=L00" % state.code)
     )
     form.is_valid()
     assert "location" in form.errors
@@ -169,33 +169,44 @@ def test_location_field__with_other_state__field_error_reported(state, location)
 def test_location_field__with_other_county__field_error_reported(county, location):
     form = LocationFilter(
         show_country=False,
-        data={"country": county.code[::-1], "location": location.identifier},
+        data=QueryDict("county=%s&location=L00" % county.code)
     )
     form.is_valid()
     assert "location" in form.errors
 
 
 def test_location_field__with_valid_code__filter_is_defined(location):
-    form = LocationFilter(show_country=False, data={"location": location.identifier})
+    form = LocationFilter(show_country=False, data=QueryDict("location=%s" % location.identifier))
     form.is_valid()
-    assert Q(location__identifier=location.identifier) == form.get_filters()
+    assert Q(location__identifier__in=[location.identifier]) == form.get_filters()
+
+
+def test_hotspot_field__with_valid_data__filter_is_defined(db_no_rollback):
+    form = LocationFilter(show_country=False, data=QueryDict("hotspot=True"))
+    form.is_valid()
+    assert Q(location__hotspot="True") == form.get_filters()
+
+
+def test_hotspot_field__invalid_boolean__field_error_reported(db_no_rollback):
+    form = LocationFilter(show_country=False, data=QueryDict("hotspot=Tru"))
+    assert "hotspot" in form.errors
 
 
 def test_observer_field__with_valid_code__form_is_valid(observer):
-    form = ObserverFilter(data={"observer": observer.identifier})
+    form = ObserverFilter(data=QueryDict("observer=%s" % observer.identifier))
     assert form.is_valid()
 
 
 def test_observer_field__with_invalid_code__form_is_invalid(observer):
     form = ObserverFilter(
-        data={"observer": observer.identifier[::-1]}
+        data=QueryDict("observer=%s" % observer.identifier[::-1])
     )
     assert not form.is_valid()
 
 
 def test_observer_field__with_invalid_code__field_error_reported(observer):
     form = ObserverFilter(
-        data={"observer": observer.identifier[::-1]}
+        data=QueryDict("observer=%s" % observer.identifier[::-1])
     )
     form.is_valid()
     assert "observer" in form.errors
@@ -203,123 +214,111 @@ def test_observer_field__with_invalid_code__field_error_reported(observer):
 
 def test_observer_field__with_lowercase_code__field_error_reported(observer):
     form = ObserverFilter(
-        data={"observer": observer.identifier.lower()}
+        data=QueryDict("observer=%s" % observer.identifier.lower())
     )
     form.is_valid()
     assert "observer" in form.errors
 
 
 def test_observer_field__with_valid_code__filter_is_defined(observer):
-    form = ObserverFilter(data={"observer": observer.identifier})
+    form = ObserverFilter(data=QueryDict("observer=%s" % observer.identifier))
     form.is_valid()
     assert Q(observer__identifier=observer.identifier) == form.get_filters()
 
 
 def test_start_field__invalid_date__field_error_reported(db_no_rollback):
-    form = DateRangeFilter(data={"start": "2025-01"})
+    form = DateRangeFilter(data=QueryDict("start=%s" % "2025-01"))
     form.is_valid()
     assert "start" in form.errors
 
 
 def test_start_field__after_finish_date__field_error_reported(db_no_rollback):
     form = DateRangeFilter(
-        data={"start": "2025-01-02", "finish": "2025-01-01"}
+        data=QueryDict("start=%s&finish=%s" % ("2025-01-02", "2025-01-01"))
     )
     form.is_valid()
     assert "start" in form.errors
 
 
 def test_start_field__with_valid_date__filter_is_defined(db_no_rollback, last_week):
-    form = DateRangeFilter(data={"start": last_week})
+    form = DateRangeFilter(data=QueryDict("start=%s" % last_week))
     form.is_valid()
     assert Q(date__gte=last_week) == form.get_filters()
 
 
 def test_finish_field__invalid_date__field_error_reported(db_no_rollback):
-    form = DateRangeFilter(data={"finish": "2025-01"})
+    form = DateRangeFilter(data=QueryDict("finish=%s" % "2025-01"))
     form.is_valid()
     assert "finish" in form.errors
 
 
 def test_finish_field__with_valid_date__filter_is_defined(db_no_rollback, last_week):
-    form = DateRangeFilter(data={"finish": last_week})
+    form = DateRangeFilter(data=QueryDict("finish=%s" % last_week))
     form.is_valid()
     assert Q(date__lte=last_week) == form.get_filters()
 
 
-def test_hotspot_field__invalid_boolean__field_error_reported(db_no_rollback):
-    form = HotspotFilter(data={"hotspot": "Tru"})
-    form.is_valid()
-    assert "hotspot" in form.errors
-
-
-def test_hotspot_field__with_valid_data__filter_is_defined(db_no_rollback):
-    form = HotspotFilter(data={"hotspot": True})
-    form.is_valid()
-    assert Q(location__hotspot="True") == form.get_filters()
-
-
 def test_category_field__invalid_choice__field_error_reported(db_no_rollback):
-    form = CategoryFilter(data={"category": "spuh"})
+    form = CategoryFilter(data=QueryDict("category=spuh"))
     form.is_valid()
     assert "category" in form.errors
 
 
 def test_category_field__with_valid_data__filter_is_defined(db_no_rollback):
-    form = CategoryFilter(data={"category": "species"})
+    form = CategoryFilter(data=QueryDict("category=species"))
     form.is_valid()
     assert Q(species__category="species") == form.get_filters()
 
 
 def test_checklist_order_field__default_order(db_no_rollback):
-    form = ChecklistOrder(data={})
+    form = ChecklistOrder(data=QueryDict(""))
     form.is_valid()
     assert "-started" in form.get_ordering()
 
 
 def test_checklist_order_field__ordering_set(db_no_rollback):
-    form = ChecklistOrder(data={"order": "-species_count"})
+    form = ChecklistOrder(data=QueryDict("order=%s" % "-species_count,-started"))
     form.is_valid()
-    assert "-species_count" in form.get_ordering()
+    assert ["-species_count", "-started"] == form.get_ordering()
 
 
 def test_checklist_order_field__invalid_order__field_error_reported(db_no_rollback):
-    form = ChecklistOrder(data={"order": "-species_coun"})
+    form = ChecklistOrder(data=QueryDict("order=%s" % "-species_coun"))
     form.is_valid()
     assert "order" in form.errors
 
 
 def test_observation_order_field__default_order(db_no_rollback):
-    form = ObservationOrder(data={})
+    form = ObservationOrder(data=QueryDict(""))
     form.is_valid()
     assert "-started" in form.get_ordering()
 
 
 def test_observation_order_field__ordering_set(db_no_rollback):
-    form = ObservationOrder(data={"order": "-count"})
+    form = ObservationOrder(data=QueryDict("order=%s" % "-count"))
     form.is_valid()
     assert "-count" in form.get_ordering()
 
 
 def test_observation_order_field__invalid_order__field_error_reported(db_no_rollback):
-    form = ObservationOrder(data={"order": "-coun"})
+    form = ObservationOrder(data=QueryDict("order=%s" % "-coun"))
     form.is_valid()
     assert "order" in form.errors
 
 
-def test_seen_order_field__default_order(db_no_rollback):
-    form = SeenOrder(data={})
+def test_species_order_field__default_order(db_no_rollback):
+    form = SpeciesOrder(data=QueryDict(""))
     form.is_valid()
-    assert ("species", "date") == form.get_ordering()
+    assert ("species", "started") == form.get_ordering()
 
 
-def test_seen_order_field__ordering_set(db_no_rollback):
-    form = SeenOrder(data={"order": "-seen"})
+def test_species_order_field__ordering_set(db_no_rollback):
+    form = SpeciesOrder(data=QueryDict("order=%s" % "species,started"))
     form.is_valid()
-    assert ("species", "-date") == form.get_ordering()
+    assert ["species", "started"] == form.get_ordering()
 
 
-def test_seen_order_field__invalid_order__field_error_reported(db_no_rollback):
-    form = SeenOrder(data={"order": "-see"})
+def test_species_order_field__invalid_order__field_error_reported(db_no_rollback):
+    form = SpeciesOrder(data=QueryDict("order=%s" % "species,-starte"))
     form.is_valid()
     assert "order" in form.errors
