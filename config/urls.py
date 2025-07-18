090809061d2695f479bed25100b85ff691e353ae
Stuart MacKay <smackay@flagstonesoftware.com>
2025-07-18 13:54:57 +0100

    Robots - Set the content-type for the sitemap

--- config/urls.py
+++ config/urls.py
@@ -26,7 +26,7 @@ urlpatterns += [
     path("robots.txt",
          TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
     path("sitemap.xml",
-         TemplateView.as_view(template_name="sitemap.xml", content_type="text/plain")),
+         TemplateView.as_view(template_name="sitemap.xml", content_type="text/xml")),
 ]
 
 if settings.DEBUG:
