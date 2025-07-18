9995469eb59a6b65ff859a4210eaf1b08ee7350a
Stuart MacKay <smackay@flagstonesoftware.com>
2025-07-18 15:34:42 +0100

    Don't cache the Checklists, Observations or Species pages
    Otherwise we are simply filling the cache with one-off pages.

--- apps/checklists/urls.py
+++ apps/checklists/urls.py
@@ -1,15 +1,9 @@
 from django.urls import path, re_path
-from django.views.decorators.cache import cache_page
 
 from .views import ChecklistsView
-
-# Cache expiration intervals
-minutes: int = 60
-hours: int = 60 * minutes
-days: int = 24 * hours
 
 app_name = "checklists"
 
 urlpatterns = [
-    path("", cache_page(1 * hours)(ChecklistsView.as_view()), name="list"),
+    path("", ChecklistsView.as_view(), name="list"),
 ]
--- apps/species/urls.py
+++ apps/species/urls.py
@@ -1,15 +1,9 @@
 from django.urls import path
-from django.views.decorators.cache import cache_page
 
 from .views import SpeciesView
-
-# Cache expiration intervals
-minutes: int = 60
-hours: int = 60 * minutes
-days: int = 24 * hours
 
 app_name = "species"
 
 urlpatterns = [
-    path("", cache_page(1 * hours)(SpeciesView.as_view()), name="list"),
+    path("", SpeciesView.as_view(), name="list"),
 ]
